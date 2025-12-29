db506aa6a6b5a78465925fdf45ed3d6a1aca09f1
lotusmito <lotuszai@gmail.com>
2025-12-29 08:07:26 +0800

    Add base path for deployment
    Set base path for deployment in Vite config.

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
+    base: '/Photo-visualtype-text-pro/', // 這裡填入你的倉庫名稱，前後都要斜線
+    plugins: [react()],
     const env = loadEnv(mode, '.', '');
     return {
       server: {
