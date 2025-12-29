0467cf0e0e2f7807fbf1bfe9c5b9907ecc83df38
lotusmito <lotuszai@gmail.com>
2025-12-29 08:08:25 +0800

    Update Vite configuration by removing base and plugins
    Remove base URL and plugins from Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,6 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-    base: '/Photo-visualtype-text-pro/', // 這裡填入你的倉庫名稱，前後都要斜線
-    plugins: [react()],
     const env = loadEnv(mode, '.', '');
     return {
       server: {
